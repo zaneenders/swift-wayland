d0aaf17520a122c031866244fb144c8a09469dac
zane <39070793+zaneenders@users.noreply.github.com>
2025-12-26 17:00:59 -0700

    .

--- Tests/LayoutTests.swift
+++ Tests/LayoutTests.swift
@@ -9,30 +9,38 @@ func layout() {
   var sizer = SizeWalker()
   let test = LayoutTest()
   test.walk(with: &sizer)
-  print(sizer.sizes)
+  for (id, element) in sizer.elements {
+    print(id, element)
+  }
+}
+
+enum Size {
+  case unknown
+  case known(height: UInt, width: UInt)
 }
 
 struct SizeWalker: Walker {
   var currentId: Hash = 0
-  var sizes: [Hash: (height: UInt, width: UInt)] = [:]
+  var elements: [Hash: Size] = [:]
 
   mutating func before(_ block: some Block) {
     if let rect = block as? Rect {
-
+      let width = rect.width * rect.scale
+      let height = rect.height * rect.scale
+      elements[currentId] = .known(height: height, width: width)
     } else if let text = block as? Word {
       guard !text.label.contains("\n") else {
         fatalError("New lines not supported yet")
       }
-      print(currentId, text.label)
       let width = UInt(text.label.count) * text.scale
       let height = 1 * text.scale
-      sizes[currentId] = (height, width)
+      elements[currentId] = .known(height: height, width: width)
+    } else {
+      elements[currentId] = .unknown
     }
   }
 
   mutating func after(_ block: some Block) {}
-
   mutating func before(child block: some Block) {}
-
   mutating func after(child block: some Block) {}
 }
