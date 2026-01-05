fd7e7e03995791dc58288b9e13b76dc2d3e6610d
Zane Enders <39070793+zaneenders@users.noreply.github.com>
2026-01-04 20:35:44 -0700

    Display frame rate (#29)

--- Sources/SwiftWayland/Demo/Screen.swift
+++ Sources/SwiftWayland/Demo/Screen.swift
@@ -2,9 +2,20 @@ import Wayland
 
 struct Screen: Block {
   let ips: [String]
+  let fps: String
+
+  init(ips: [String], fps: String = "") {
+    self.ips = ips
+    self.fps = fps
+  }
+
   var layer: some Block {
     Direction(.vertical) {  // TODO: This group should be implict
       EmptyBlock()
+      if !fps.isEmpty {
+        Text(fps)
+          .foreground(.green)
+      }
       Text("Zane was here")
         .foreground(.cyan)
       Layout()
