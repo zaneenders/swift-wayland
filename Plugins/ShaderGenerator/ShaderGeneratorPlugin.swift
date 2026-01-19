c6c559e7a15c163b6cc93d45142d382734829534
zane <39070793+zaneenders@users.noreply.github.com>
2026-01-19 16:50:52 -0700

    .

--- Plugins/ShaderGenerator/ShaderGeneratorPlugin.swift
+++ Plugins/ShaderGenerator/ShaderGeneratorPlugin.swift
@@ -3,15 +3,13 @@ import PackagePlugin
 @main
 struct ShaderGeneratorPlugin: BuildToolPlugin {
   func createBuildCommands(context: PluginContext, target: Target) throws -> [Command] {
-    // This plugin should only run for the Wayland target
+
     guard target.name == "Wayland" else { return [] }
 
-    // Paths for the shader files and output
     let packageDirectory = context.package.directory
     let shadersDirectory = packageDirectory.appending("shaders")
     let outputFile = context.pluginWorkDirectory.appending("Shaders.swift")
 
-    // Get the generator tool
     let generatorTool = try context.tool(named: "ShaderGeneratorTool")
 
     return [
